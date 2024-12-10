6adf6fdc7077fcbec922af7f8e2104858d612c8e
Puslitbang <puslitbang@Puslitbangs-MacBook-Pro.local>
2024-12-10 19:34:58 +0700

    feat:adding cors

--- middlewares/cors.go
+++ middlewares/cors.go
@@ -1,1 +1,25 @@
 package middlewares
+
+import (
+	"os"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func CORSConfig() echo.MiddlewareFunc {
+	origins := []string{os.Getenv("PRODUCTION_CLIENT_URL")}
+	if os.Getenv("APP_ENV") == "development" {
+		origins = append(origins, os.Getenv("DEVELOPMENT_CLIENT_URL"))
+	}
+
+	return middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: origins,
+		AllowHeaders: []string{
+			echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization,
+		},
+		AllowMethods: []string{
+			echo.GET, echo.HEAD, echo.PUT, echo.PATCH, echo.POST, echo.DELETE, echo.OPTIONS,
+		},
+	})
+}
